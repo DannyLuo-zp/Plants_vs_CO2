6ed58d4ac6aed2abb7212d3b22e28a6b0708d4c1
DannyLuo-zp <dannyluozp@gmail.com>
2020-10-25 20:04:16 +0800

    add pages

--- eco_app/src/Map.js
+++ eco_app/src/Map.js
@@ -1,15 +1,122 @@
 import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript,Rectangle } from '@react-google-maps/api';
+import {Modal} from 'antd';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
+  width: '100%',
+    height: '700px',
+    display: 'flex',  
+    justifyContent:'center', 
+    alignItems:'center'
+}
 
 const center = {
-  lat: -3.745,
-  lng: -38.523
-};
+  lat: 41.881690,
+  lng: -87.630387
+  //41.881690, -87.630387
+}
+const areaBounds = { 
+  south: 41.867131,
+  east: -87.624032
+}
+
+const areaSize = {
+  height: 0.02,
+  width: 0.02
+}
+
+function Options(count){
+      return {
+          strokeOpacity: 0.25,
+          fillColor: 'green',
+          fillOpacity: 0.05 + (count) * 0.05
+          //Math.min(0.05 + (count) * 0.05, 0.3)
+      }
+}
+
+const rectOptions = {
+    strokeOpacity: 0.3,
+    fillOpacity: 0.1
+}
+
+const gridSize = 8
+const zoomRate = 14
+
+
+class Grid extends React.Component {
+  constructor(props){
+      super(props);
+      this.bound = props.bound;
+  }
+
+  state = { 
+      visible: false,
+      count: Math.floor(Math.random() * Math.floor(10))
+  };
+
+  showModal = () => {
+      this.setState({
+      visible: true,
+      });
+  };
+
+  handleOk = e => {
+      this.setState({count: this.state.count + 1});
+  };
+
+  handleCancel = e => {
+      this.setState({
+      visible: false,
+      });
+  };
+
+  diminish() {
+      this.setState({state: 
+          (this.state.count > 0)? this.state.count - 1: 0 });
+  }
+
+  render() {
+      return (
+      <>
+          <Rectangle bounds={this.bound} 
+          options={Options(this.state.count)} 
+          onClick={this.showModal}/>
+          <Modal
+          title="Area"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          okText="Add another Plant"
+          >
+          <p>
+              Coordinate: 
+          </p>
+          <p>
+              North - {this.bound.north}, 
+              South - {this.bound.south},
+          </p>
+          <p>
+              East - {this.bound.east},
+              West - {this.bound.west}
+          </p>
+          <p>Current Plants number: {this.state.count}</p>
+          </Modal>
+      </>
+      );
+  }
+}
+
+
+
+function option(e, south, east){
+  let ans = {
+      north: south + areaSize.height,
+      south: south,
+      east: east - e * areaSize.width,
+      west: east - (e + 1) * areaSize.width   
+  }
+  return ans
+}
 
 export function EcoMap() {
   return (
@@ -17,15 +124,16 @@ export function EcoMap() {
       googleMapsApiKey="AIzaSyBnQLtnVmmFchhoVNletHvubUqEX9uGT88"
     >
       <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
+    mapContainerStyle={containerStyle}
+    zoom={zoomRate}
+    center={center}
+  >
+    <Grid bound={option(0, areaBounds.south, areaBounds.east)} />
+    <Grid bound={option(1, areaBounds.south, areaBounds.east)} />
+  </GoogleMap>
     </LoadScript>
   )
 }
 
 export default React.memo(EcoMap)
+  
